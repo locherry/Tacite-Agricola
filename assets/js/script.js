7082c2adc8fab5495e8e6c7d7afe1b6016dd7fa5
Bob Stark <aloyscherry@gmail.com>
2023-04-27 19:44:44 +0200

    small improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -247,6 +247,8 @@ const observer = new IntersectionObserver((entries) => {
         link.classList.remove("active");
       });
       linkList[sectionList.indexOf(entry.target)].classList.add("active");
+      console.log(linkList[sectionList.indexOf(entry.target)]);
+      window.history.replaceState({}, null, linkList[sectionList.indexOf(entry.target)].getAttribute('href'));
     }
   }
   if (!document.querySelector(".nav-link").classList.contains("active")) {
@@ -272,3 +274,53 @@ document.addEventListener("DOMContentLoaded", function () {
     window.scrollTo(0, 0);
   }, 3000);
 });
+
+// typing animation
+(function typingAnimation() {
+  const typedTextSpan = document.querySelector(".typed-text");
+  const cursorSpan = document.querySelector(".cursor");
+
+  // const textArray = ["hard", "fun", "a journey", "LIFE"];
+  const textArray = typedTextSpan.getAttribute("data-text").split("; ");
+  const typingDelay = 200;
+  const erasingDelay = 100;
+  const newTextDelay = 2000; // Delay between current and next text
+  let textArrayIndex = 0;
+  let charIndex = 0;
+
+  function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+      if (!cursorSpan.classList.contains("typing"))
+        cursorSpan.classList.add("typing");
+      typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingDelay);
+    } else {
+      cursorSpan.classList.remove("typing");
+      setTimeout(erase, newTextDelay);
+    }
+  }
+
+  function erase() {
+    if (charIndex > 0) {
+      if (!cursorSpan.classList.contains("typing"))
+        cursorSpan.classList.add("typing");
+      typedTextSpan.textContent = textArray[textArrayIndex].substring(
+        0,
+        charIndex - 1
+      );
+      charIndex--;
+      setTimeout(erase, erasingDelay);
+    } else {
+      cursorSpan.classList.remove("typing");
+      textArrayIndex++;
+      if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+      setTimeout(type, typingDelay + 1100);
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // On DOM Load initiate the effect
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+  });
+})();
